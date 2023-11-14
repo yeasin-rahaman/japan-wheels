ec35a8485b31e7ea270270c0f854d96aa85f5995
Yeasin Rahaman <santo9585@gmail.com>
2023-11-15 00:16:16 +0600

    'test1'

--- app/(blog)/add-blog/page.js
+++ app/(blog)/add-blog/page.js
@@ -1,21 +1,4 @@
 "use client";
-import Footer from "@/app/components/common/Footer";
-import DefaultHeader from "@/app/components/common/DefaultHeader";
-import HeaderSidebar from "@/app/components/common/HeaderSidebar";
-import HeaderTop from "@/app/components/common/HeaderTop";
-import MobileMenu from "@/app/components/common/MobileMenu";
-import LoginSignupModal from "@/app/components/common/login-signup";
-import Image from "next/image";
-import Blog from "@/app/components/common/Blog";
-import Meta from "@/app/components/blog/blog-single/Meta";
-import Blockquote from "@/app/components/blog/blog-single/Blockquote";
-import Features from "@/app/components/blog/blog-single/Features";
-import Requirements from "@/app/components/blog/blog-single/Requirements";
-import Share from "@/app/components/blog/blog-single/Share";
-import Tag from "@/app/components/blog/blog-single/Tag";
-import Pagination from "@/app/components/blog/blog-single/Pagination";
-import Comments from "@/app/components/blog/blog-single/Comments";
-import CommentForm from "@/app/components/blog/blog-single/CommentForm";
 import AddBlog from "./AddBlog";
 import PageNotFound from "@/utils/PageNotFound";
 
@@ -25,16 +8,22 @@ const metadata = {
 };
 
 const BlogDynamicSingle = () => {
-  const Authorization= localStorage.getItem("jwt_access_token");
+
+
+  if (typeof window !== 'undefined') {
+    const Authorization = localStorage.getItem("jwt_access_token");
+    const item = localStorage.getItem('key')
+  }
+
 
   return (
     <div className="wrapper pb-80 pt-80">
-    {
-      Authorization ?  <AddBlog /> : <PageNotFound/>
-    }
+      {
+        Authorization ? <AddBlog /> : <PageNotFound />
+      }
 
 
-    
+
     </div>
     // End wrapper
   );
--- app/OurComponents/home/purchaseFlow/PurchaseFlow.js
+++ app/OurComponents/home/purchaseFlow/PurchaseFlow.js
@@ -43,7 +43,7 @@ function PurchaseFlow() {
           width={35}
           height={35}
           className="arrow"
-          src="/images/purchaseFlow/right-angle.svg"
+          src="{}"
         />
         <div className="step1">
           <div className="step-left">
@@ -63,7 +63,7 @@ function PurchaseFlow() {
             <Image
               width={60}
               height={40}
-              src="/images/purchaseFlow/credit-card.svg"
+              src={"/images/purchaseFlow/.svg"}
             />
             <div className="sentence-zone">Pay via your preferred method</div>
           </div>
