e5d8c91d1412cf9dd45c533bc6ef1253436503fa
Yeasin Rahaman <santo9585@gmail.com>
2023-12-26 01:55:08 +0600

    update

--- app/OurComponents/listing/CarItems.js
+++ app/OurComponents/listing/CarItems.js
@@ -4,129 +4,143 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useEffect } from 'react';
 const CarItems = () => {
+  const [listingCars, setListingCars] = useState([]);
 
   useEffect(() => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow'
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.japanwheels.com/api/vehicles?count=10');
+        const data = await response.json();
+        setListingCars(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
-    fetch("http://78.46.90.228/api/?json&code=JWLgF5h4kLs3&sql=select%20*%20from%20main%20limit%2010", requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-  }, [])
+    fetchData();
+  }, []); // The empty dependency array ensures this effect runs only once on component mount
+
 
   return (
     <>
 
-      {listingCar.slice(0, 9).map((listing) => (
-        <div className="col-sm-6 col-xl-4" key={listing.id}>
-          <Link href={`listings/${listing.id}`}>
-            <div className="car-listing">
-              <div className="thumb">
-                {listing.featured ? (
-                  <>
-                    <div className="tag">FEATURED</div>
-                  </>
-                ) : undefined}
-                {!listing.featured ? (
-                  <>
-                    <div className="tag blue">SPECIAL</div>
-                  </>
-                ) : undefined}
 
-                <Image
-                  width={284}
-                  height={183}
-                  style={{
-                    width: "100%",
-                    height: "10rem",
-                    objectFit: "cover",
-                  }}
-                  priority
-                  src={listing.image}
-                  alt={listing.title}
-                />
-                <div className="thmb_cntnt2">
-                  <ul className="mb0">
-                    <li className="list-inline-item">
-                      <a className="text-white" href="#">
-                        <span className="flaticon-photo-camera mr3" />{" "}
-                        {listing.photosCount}
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a className="text-white" href="#">
-                        <span className="flaticon-play-button mr3" />{" "}
-                        {listing.videosCount}
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <div className="thmb_cntnt3">
-                  <ul className="mb0">
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-shuffle-arrows" />
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-heart" />
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+      {
+        listingCars.slice(0, 9).map((listing) => (
+          <div className="col-sm-6 col-xl-4" key={listing.ID}>
+            <Link href={`listings/${listing.ID}`}>
+              <div className="car-listing">
+                <div className="thumb">
+                  {listing.AUCTION ? (
+                    <>
+                      <div className="tag">FEATURED</div>
+                    </>
+                  ) : undefined}
+                  {!listing.AUCTION ? (
+                    <>
+                      <div className="tag blue">SPECIAL</div>
+                    </>
+                  ) : undefined}
 
-              <div className="details">
-                <div className="wrapper">
-                  <h5 className="price">${listing.price}</h5>
-                  <h6 className="title">
-                    {listing.title}
-                  </h6>
-                  <div className="listign_review">
+
+
+
+
+                  < Image
+
+                    width={284}
+                    height={183}
+                    style={{
+                      width: "100%",
+                      height: "10rem",
+                      objectFit: "cover",
+                    }}
+                    priority
+                    src={listing.IMAGES.split('#')[0]}  // Use only the first image URL
+                    alt={""}
+                  />
+
+
+
+
+
+
+                  <div className="thmb_cntnt2">
                     <ul className="mb0">
-                      {[...Array(5)].map((_, index) => (
-                        <li key={index} className="list-inline-item">
-                          <a href="#">
-                            <i className="fa fa-star" />
-                          </a>
-                        </li>
-                      ))}
                       <li className="list-inline-item">
-                        <a href="#">{listing.rating}</a>
+                        <a className="text-white" href="#">
+                          <span className="flaticon-photo-camera mr3" />{" "}
+                          {listing.YEAR}
+                        </a>
+                      </li>
+
+                    </ul>
+                  </div>
+                  <div className="thmb_cntnt3">
+                    <ul className="mb0">
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-shuffle-arrows" />
+                        </a>
                       </li>
                       <li className="list-inline-item">
-                        ({listing.reviewsCount} reviews)
+                        <a href="#">
+                          <span className="flaticon-heart" />
+                        </a>
                       </li>
                     </ul>
                   </div>
                 </div>
-                {/* End wrapper */}
 
-                <div className="listing_footer">
-                  <ul className="mb0">
-                    <li className="list-inline-item">
-                      <span className="flaticon-road-perspective me-2" />
-                      {listing.mileage}
-                    </li>
-                    <li className="list-inline-item">
-                      <span className="flaticon-gas-station me-2" />
-                      {listing.fuelType}
-                    </li>
-                    <li className="list-inline-item">
-                      <span className="flaticon-gear me-2" />
-                      {listing.transmission}
-                    </li>
-                  </ul>
+                <div className="details">
+                  <div className="wrapper">
+                    <h5 className="price">{listing.MODEL_NAME}</h5>
+                    <h6 className="title">
+                      {listing.YEAR}
+                    </h6>
+                    <div className="listign_review">
+                      <ul className="mb0">
+                        {[...Array(5)].map((_, index) => (
+                          <li key={index} className="list-inline-item">
+                            <a href="#">
+                              <i className="fa fa-star" />
+                            </a>
+                          </li>
+                        ))}
+                        <li className="list-inline-item">
+                          <a href="#">{listing.RATE}</a>
+                        </li>
+                        <li className="list-inline-item">
+                          ({listing.RATE} reviews)
+                        </li>
+                      </ul>
+
+                    </div>
+                  </div>
+                  {/* End wrapper */}
+
+                  <div className="listing_footer">
+                    <ul className="mb0">
+                      <li className="list-inline-item">
+                        <span className="flaticon-road-perspective me-2" />
+                        {listing.MILEAGE}
+                      </li>
+                      <li className="list-inline-item">
+                        <span className="flaticon-gas-station me-2" />
+                        {listing.MODEL_ID}
+                      </li>
+                      <li className="list-inline-item">
+                        <span className="flaticon-gear me-2" />
+                        {listing.ENG_V}
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      ))}
+            </Link>
+          </div>
+        ))
+      }
 
       <div className="col-lg-12">
         <div className="mbp_pagination mt20">
--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['api.japanwheels.com'],
-      },
+  images: {
+    domains: ['api.japanwheels.com', '8.ajes.com'],
+  },
 };
 
 module.exports = nextConfig;
