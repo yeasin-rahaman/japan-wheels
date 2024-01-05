c2a6d9cc719fbc008fdf1f54b186d8fe5ae3e4ae
Yeasin Rahaman <santo9585@gmail.com>
2024-01-06 01:14:34 +0600

    update

--- app/OurComponents/home/Blog.js
+++ app/OurComponents/home/Blog.js
@@ -12,55 +12,61 @@ const Blog = () => {
           data-aos="fade-up"
           data-aos-delay="100"
         >
-          <div className="for_blog">
-            <div className="thumb">
-              <div className="tag">{post.tag}</div>
-              <Image
-                width={394}
-                height={254}
-                style={{ objectFit: "cover" }}
-                className="img-whp"
-                src={post.imgSrc}
-                alt={post.title}
-              />
-            </div>
-            <div className="details">
-              <div className="wrapper">
-                <div className="bp_meta">
-                  <ul>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-user" />
-                        {post.author}
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-chat" />
-                        {post.numComments} Comments
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#">
-                        <span className="flaticon-calendar-1" />
-                        {post.date}
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-                <h4 className="title">
-                  <Link href={`/blogs/${post?.id}`}>{post.title}</Link>
-                </h4>
-                <Link href={`/blogs/${post?.id}`} className="more_listing">
+
+          <Link href={`/blogs/${post?.id}`} className="more_listing">
+
+
+            <div className="for_blog">
+              <div className="thumb">
+                <div className="tag">{post.tag}</div>
+                <Image
+                  width={394}
+                  height={254}
+                  style={{ objectFit: "cover" }}
+                  className="img-whp"
+                  src={post.imgSrc}
+                  alt={post.title}
+                />
+              </div>
+              <div className="details">
+                <div className="wrapper">
+                  <div className="bp_meta">
+                    <ul>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-user" />
+                          {post.author}
+                        </a>
+                      </li>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-chat" />
+                          {post.numComments} Comments
+                        </a>
+                      </li>
+                      <li className="list-inline-item">
+                        <a href="#">
+                          <span className="flaticon-calendar-1" />
+                          {post.date}
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                  <h4 className="title">
+                    <Link href={`/blogs/${post?.id}`}>{post.title}</Link>
+                  </h4>
+
                   Read More{" "}
                   <span className="icon">
                     <span className="fas fa-plus" />
                   </span>
-                </Link>
+
+                </div>
               </div>
             </div>
-          </div>
+          </Link >
         </div>
+
       ))}
     </>
   );
